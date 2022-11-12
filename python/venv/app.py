f89d0520932f1a012d7fb943f9047d26c91acd42
Waldow <lwaldow2@gmail.com>
2022-11-12 17:37:29 -0500

    Add starter code for model

--- python/venv/app.py
+++ python/venv/app.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+"""
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline 
+
+# we need to load a tokenizer and the model, you can usually find this on the model card
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+
+inputs = ["i hate you piyush im going to kill you"] 
+
+pipe = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+result = pipe(inputs)
+print(result)
+"""
+
 if __name__ == "__main__":
     app.run()

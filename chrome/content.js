8442ebb556e9cbd49e2e3df225daff7f33f96eb6
nubDotDev <nubdotdev@gmail.com>
2022-11-12 21:30:13 -0500

    Add the entire functionality of the extension

--- chrome/content.js
+++ chrome/content.js
@@ -3,11 +3,17 @@ let enabled = false;
 const censorStyle = document.createElement("style");
 censorStyle.classList.add("censor-style");
 censorStyle.innerHTML = `
-color: transparent;
-text-shadow: 0 0 5px rgba(0,0,0,0.5);
+.censored {
+    color: transparent;
+    text-shadow: 0 0 10px rgba(0,0,0,0.5);
+    -webkit-user-select: none; /* Safari */        
+    -moz-user-select: none; /* Firefox */
+    -ms-user-select: none; /* IE10+/Edge */
+    user-select: none; /* Standard */
+}
 `;
 
-chrome.runtime.sendMessage({title: "get-enabled"}, (res) => {
+chrome.runtime.sendMessage({ title: "get-enabled" }, (res) => {
     enabled = res;
     if (enabled) censor();
     else decensor();
@@ -23,31 +29,40 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 });
 
 function censor() {
+    // fetch("http://127.0.0.1:5000/sentiment-analysis").then(res => res.json()).then(console.log);
     if (document.getElementsByTagName("censored").length === 0) {
         const toCensor = getCensorable();
-        const sentences = toCensor.map(p => separateParagraph(p));
-        const censored = sentences.map(p => p.map(s => sentiment(s) <= -0.5 ? s : "<span class='censored'>" + s + "</span>"));
-        toCensor.array.forEach((p, i) => {
-            p.innerHTML = censored[i].join("");
+        const sentences = toCensor.map(p => separateParagraph(p.innerHTML));
+        Promise.all(sentences.map(p => Promise.all(p.map(s => sentiment(s).then(res => res >= 0 ? s : "<span class='censored'>" + s + "</span>"))))).then(res => {
+            toCensor.forEach((p, i) => {
+                p.innerHTML = res[i].join(". ");
+            });
+            document.body.appendChild(document.createElement("censored"));
         });
-        document.body.appendChild(document.createElement("censored"));
     }
     if (document.getElementsByClassName("censor-style").length === 0)
         document.head.appendChild(censorStyle);
 }
 
 function decensor() {
-    document.getElementsByClassName("censor-style").array.forEach(e => e.remove());
+    Array.from(document.getElementsByClassName("censor-style")).forEach(e => e.remove());
 }
 
 function getCensorable() {
-    return document.getElementsByTagName("p");
+    return Array.from(document.getElementsByTagName("p"));
 }
 
 function separateParagraph(p) {
-    return p.split(/[,.]/);
+    return p.split('. ');
 }
 
 function sentiment(s) {
-    return Math.random() * 2 - 1;
+    // return Math.random() * 2 - 1;
+    return fetch("http://127.0.0.1:5000/sentiment-analysis", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json; charset=utf8"
+        },
+        body: JSON.stringify({text: s})
+    }).then(res => res.json()).then(res => (res[0].label === "POSITIVE" ? 1 : -1) * res[0].score);
 }
--- python/app.py
+++ python/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import pipeline 
 
 app = Flask(__name__)
+CORS(app)
 
 # we need to load a tokenizer and the model, you can usually find this on the model card
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
@@ -10,15 +12,20 @@ model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-unca
 
 @app.route("/")
 def index():
-    return "Error"
+    print("request.json")
+    # if request.method != "POST":
+    #     return "Error"
+    inputs = ["I am happy when I kill", "you are the sweetest person ever"] 
+    pipe = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+    result = pipe(inputs)
+    return result
 
 @app.route("/sentiment-analysis", methods=["GET", "POST"])
 def sentiment_analysis():
-    print("AHAHAHAH")
-    print(request.json)
     if request.method != "POST":
         return "Error"
-    inputs = ["I am happy when I kill", "you are the sweetest person ever"] 
+    # inputs = ["I am happy when I kill", "you are the sweetest person ever"] 
+    inputs = [request.get_json()["text"]]
     pipe = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
     result = pipe(inputs)
     return result

6ee3497d73279a1a56cd6e5c39c9645124b9f9ca
Waldow <lwaldow2@gmail.com>
2022-11-12 20:52:42 -0500

    Write front end interface and logic

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,14 +1,27 @@
-const enabled = document.getElementById("enabled");
+const enabledBtn = document.getElementById("enabled");
+const enabledLabel = document.getElementById("enabled-label");
 
-enabled.addEventListener("change", () => {
+function doCheck(isChecked) {
+    localStorage.setItem('checked', String(isChecked));
+    enabledLabel.innerText = isChecked ? "negative vibes FILTERED" : "filtering OFF";
     chrome.tabs.query({currentWindow: true, active: true}, tabs =>
-        chrome.tabs.sendMessage(tabs[0].id, {title: "toggle", value: enabled.checked}, (res) => chrome.runtime.lastError)
+        chrome.tabs.sendMessage(tabs[0].id, {title: "toggle", value: isChecked}, (res) => chrome.runtime.lastError)
     );
+}
+
+//localStorage.removeItem('checked');
+let storedChecked = localStorage.getItem('checked') === "true" ? true : false;
+enabledBtn.checked = storedChecked;
+doCheck(storedChecked);
+
+enabledBtn.addEventListener("change", function (event) {
+    let isChecked = enabledBtn.checked;
+    doCheck(isChecked);
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.title === "get-enabled") {
-        enabled = message.value || false;
+        let enabled = message.value || false;
         if (enabled) censor();
         sendResponse(enabled.checked);
     }
